2fa17aa4bc465617cf1d487b0b035b2d77f94cc1
Luise Schneider <luise.schneider@berlin.de>
2018-11-10 23:47:09 +0100

    done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -2,37 +2,94 @@
 
 class HangmanGame {
   constructor() {
-
     // the array to choose a word from at random
-    this.words = ['Ironhack']
+    this.words = [
+      "GLENFIDDICH",
+      "BALVENIE",
+      "ABERFELDY",
+      "ABERLOUR",
+      "ARDBEG",
+      "ARDMORE",
+      "ARRAN",
+      "AUCHENTOSHAN",
+      "BALBLAIR",
+      "BENRIACH",
+      "BENROMACH",
+      "BOWMORE",
+      "BRUICHLADDICH",
+      "BUNNAHABHAIN",
+      "CRAIGELLACHIE",
+      "DALWHINNIE",
+      "EDRADOUR",
+      "GLENALLACHIE",
+      "GLENDRONACH",
+      "GLENFARCLAS",
+      "GLENGOYNE",
+      "JURA",
+      "LAGAVULIN",
+      "LAPHROAIG",
+      "MORTLACH",
+      "SPRINGBANK",
+      "TALISKER",
+      "TOBERMORY",
+      "TULLIBARDINE"
+    ];
     // our secret word for this instance of the hangman game
-    this.secretWord = this.getWord()
+    this.secretWord = this.getWord();
 
-    this.letters = []         // all the clicked letters
-    this.guessedLetter = ''   // all the correctly guessed letters ( as a string, not an array )
-    this.errorsLeft = 10      // errors you are still allowed
+    this.letters = []; // all the clicked letters
+    this.guessedLetter = ""; // all the correctly guessed letters ( as a string, not an array )
+    this.errorsLeft = 6; // errors you are still allowed
   }
 
   // returns upper case version of a random word from our words array ( check out ._sample() from lodash :-) )
-  getWord() { /* TODO */ }
-  checkIfLetter(keyCode) { /* TODO, see the table on https://keycode.info/ */ }
-  checkClickedLetters(letter) { /* TODO */ }
+  getWord() {
+    var randomIndex = Math.floor(Math.random() * this.words.length);
+    return this.words[randomIndex];
+  }
+  checkIfLetter(keyCode) {
+    return keyCode > 64 && keyCode < 91;
+  }
+
+  checkClickedLetters(letter) {
+    if (this.letters.includes(letter)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
   // this takes the POSITION in the secret word as the parameter !
   addCorrectLetter(position) {
-    // 1. push to the clicked letters
-    // 2. add the letter to the guessedLetter string (uppercase!)
-    // 3. alert the user if they won the game !
+    this.letters.push(this.secretWord[position]);
+    this.guessedLetter += this.secretWord[position].toUpperCase();
+    // console.log(this.guessedLetter);
+    if (this.checkWinner()) {
+      alert("Sláinte, you're a real whisky expert!");
+      $("#new-game-button")
+        .parent()
+        .show();
+    }
   }
+
   addWrongLetter(letter) {
-    // 1. push to the clicked letters
-    // 2. substract from the number of errors the user is still allowed to make
-    // 3. alert the user if they lost the game !
+    this.letters.push(letter);
+    this.errorsLeft--;
+    // console.log(this.letters);
+    if (this.checkGameOver()) {
+      alert("Shit happens, try again!");
+      $("#new-game-button")
+        .parent()
+        .show();
+    }
   }
 
   // check if the user lost the game
-  checkGameOver() { /* TODO */ }
+  checkGameOver() {
+    return this.errorsLeft == 0;
+  }
   // check if the user won the game
-  checkWinner() { /* TODO : this is a bit complicated because the game designer decided to store the guessedLetter as a string, but you will manage :-) */ }
-
+  checkWinner() {
+    return this.guessedLetter.length == this.secretWord.length;
+  }
 }
